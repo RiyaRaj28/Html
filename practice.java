8ca124a95a4e864241d90d8cf7cb8626d4d36cce
Riya <Riya@itsme.com>
2023-10-14 03:06:41 +0530

    Advance selectors, Transform, MediaQueries, nthChild

---
+++ practice.java
@@ -0,0 +1,31 @@
+import java.util.*; 
+
+public class practice {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner();
+		int t = sc.nextInt();
+		
+		for(int c=1; c<=t; c++){
+		    int n = sc.nextInt(); 
+		    
+		     int[] A = new int[n]; 
+		     for(int i=0; i<n; i++){
+		         A[i] = sc.nextInt(); 
+		     }       
+		         
+		     int[] B = new int[n]; 
+		     for(int j=0; j<n; j++){
+		         B[j] = sc.nextInt();
+		     }
+		         
+		         
+		     }
+		     
+		}
+    }
+
+ 
+
+
+ 
